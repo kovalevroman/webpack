6cf6acc43931f8178cb93bf0d8fb308b23f136d1
kovalevr <koavlevroman@gmail.com>
2020-01-22 11:04:47 +0300

    Передача параметров в модули через DefinePlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+const webpack = require('webpack');
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
 module.exports = {
   mode: 'development',
   entry: './home',
@@ -5,5 +9,13 @@ module.exports = {
     filename: 'build.js',
     library: 'home'
   },
-  watch: true
+  watch: true,
+  devtool: NODE_ENV === 'development' ? 'source-map' : null,
+  plugins: [
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(NODE_ENV),
+      LANG: '"ru"'
+    })
+  ]
+
 };
--- welcome.js
+++ welcome.js
@@ -1,3 +1,9 @@
 export const welcome = (message) => {
+
+  if ( NODE_ENV === 'development'){
+    console.log('LANG', LANG);
+    console.log('development');
+  }
+
   console.log('message', message);
 };
